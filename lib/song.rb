7f3c049aaecd84e09bdb0c71f9e2f97a68edfc08
Robert McCary <robert.mccary@yahoo.com>
2019-01-28 15:28:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
-class Song 
-  attr_accessor :title :author 
-  def initialize 
+class Song
+  attr_accessor :title :author
+  def initialize
     @title=title
-    @author=author 
+    @author=author
   end
-end 
+end
     
